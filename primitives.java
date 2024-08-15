235e4efeb5b44dfde961a2f4f5b58f7f40b18f26
Therf13 <157944898+Therf13@users.noreply.github.com>
2024-08-15 12:01:44 +0000

    Finish

--- primitives.java
+++ primitives.java
@@ -1,1 +1,11 @@
-
+class primitive {
+    public static void main (String[] args){
+    int quatro = 3110;
+    byte zero = 0;
+    short num1 = 1;
+    double ver = 2.0;
+    boolean ans = true;
+    String output = "H" + quatro + " " + "w" + zero + "r" + num1 + "d" + " " + ver + " " + ans;
+    System.out.println(output);
+}
+}
